2e37fe3f2f3be1fb99ec80f195445c38c10b482f
heirani094 <142579026+heirani094@users.noreply.github.com>
2024-07-27 12:34:13 +0330

    new changes

--- blog/models.py
+++ blog/models.py
@@ -58,17 +58,17 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    email = models.EmailField
-    subject = models.CharField(max_length=255)
-    message = models.TextField()
+    name = models.CharField(max_length=255,default='ناشناس')
+    email = models.EmailField(default='info@yahoo.com')
+    subject = models.CharField(max_length=255,default=' ')
+    message = models.TextField(default='')
     approved = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
-        return self.name
+      return self.email
 
     class Meta:
-        ordering = ['-created_date']
+       ordering = ['-created_date']
--- website/forms.py
+++ website/forms.py
@@ -1,12 +1,12 @@
 # forms.py
 from django import forms
 from django.forms import ModelForm
-from captcha.fields import CaptchaField
+
 from website.models import Contact
 
 
 class ContactForm(ModelForm):
-    captcha = CaptchaField()
+
 
     class Meta:
         model = Contact
